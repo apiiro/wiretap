051539bc5f66e0ac7ba792e0367383ac0dc86688
Yoel Schejter <94913252+ravyolii@users.noreply.github.com>
2023-09-14 13:42:03 +0300

    Update docker.build.sh

--- docker.build.sh
+++ docker.build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # ENV_PREFIX=apiiro-rnd-network-broker
-  # -t gcr.io/$ENV_PREFIX/broker-agent:latest \
-  # -t gcr.io/$ENV_PREFIX/broker-agent:$AGENT_TAG \
 ENV_PREFIX=apiiro/network-broker
-AGENT_TAG=0.4.6
+AGENT_TAG=0.5.0
 docker buildx build --platform linux/amd64 \
   --push --pull \
-  -t apiiro.jfrog.io/vladimir-vuln-tests/broker-agent:0.5.0 \
+  -t gcr.io/$ENV_PREFIX/broker-agent:latest \
+  -t gcr.io/$ENV_PREFIX/broker-agent:$AGENT_TAG \
   -f wiretap-agent.Dockerfile \
   . 
